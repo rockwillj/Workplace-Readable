b528d91b8425761d2ad5b8357d10e6fff5ca2380
iwataj <iwata@sansan.com>
2017-02-06 20:46:47 +0900

    Add: highlight mentions to me in the content

--- js/content.js
+++ js/content.js
@@ -1,2 +1,13 @@
 $(function () {
+    /*
+     * Highlight mentions to me
+     */
+    var href = $('#pagelet_bluebar a[href*="profile.php"]').attr('href');
+    var match = /profile\.php\?id=(\d+)/.exec(href);
+    if (match && match.length == 2) {
+        var style = document.createElement('style');
+        document.head.appendChild(style);
+        var rule = '.profileLink[href*="' + match[1] + '"] { background: yellow; }';
+        style.sheet.insertRule(rule, 0);
+    }
 });
