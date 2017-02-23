b3ec92b751832a420d79b0edee9ea01040455daa
iwataj <iwata@sansan.com>
2017-02-23 22:30:10 +0900

    Add: Chrome Extension Options (empty)

---
+++ js/options.js
@@ -0,0 +1,14 @@
+$(function () {
+    loadOptions();
+    $('#save').click(saveOptions);
+    $('#reset').click(resetOptions);
+
+    function loadOptions() {
+    }
+
+    function saveOptions() {
+    }
+
+    function resetOptions() {
+    }
+});
