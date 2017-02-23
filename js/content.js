852b26fefac4d35d3e2c8d6e364bf7b2f1f0f9ab
iwataj <iwata@sansan.com>
2017-02-23 22:37:44 +0900

    Add: color options in “Highlight Mentions to Me”
    - use chrome.storage (get, set, clear)
    - add storage permission
    - use input[type=“color”] for usability
    - rewrite with template strings in JavaScript

--- js/content.js
+++ js/content.js
@@ -18,14 +18,22 @@ $(function () {
     /*
      * Highlight mentions to me
      */
-    var href = $('#pagelet_bluebar a[href*="profile.php"]').attr('href');
-    var match = /profile\.php\?id=(\d+)/.exec(href);
-    if (match && match.length == 2) {
-        var style = document.createElement('style');
-        document.head.appendChild(style);
-        var rule = '.profileLink[href*="' + match[1] + '"] { background: yellow; }';
-        style.sheet.insertRule(rule, 0);
-    }
+    chrome.storage.sync.get({
+        color: '#365899',
+        bgColor: '#ffff00'
+    }, function (items) {
+        var href = $('#pagelet_bluebar a[href*="profile.php"]').attr('href');
+        var match = /profile\.php\?id=(\d+)/.exec(href);
+        if (match && match.length == 2) {
+            var style = document.createElement('style');
+            document.head.appendChild(style);
+            var rule = `.profileLink[href*="${match[1]}"] {
+                 color: ${items.color} !important;
+                 background: ${items.bgColor};
+                 }`;
+            style.sheet.insertRule(rule, 0);
+        }
+    });
 });
 
 // onpage: Trigger ajax paging event
--- js/options.js
+++ js/options.js
@@ -2,13 +2,47 @@ $(function () {
     loadOptions();
     $('#save').click(saveOptions);
     $('#reset').click(resetOptions);
+    $('input[type="color"]').change(function () {
+        $(this).prev('input').val($(this).val());
+    });
 
     function loadOptions() {
+        chrome.storage.sync.get({
+            color: '#365899',
+            bgColor: '#ffff00'
+        }, function (items) {
+            $('#mentionColor').next('input').andSelf().val(items.color);
+            $('#mentionBgColor').next('input').andSelf().val(items.bgColor);
+        });
     }
 
     function saveOptions() {
+        var color = $('#mentionColor').val();
+        var bgColor = $('#mentionBgColor').val();
+        chrome.storage.sync.set({
+            color: color,
+            bgColor: bgColor
+        }, function () {
+            $('#mentionColor + input').val(color);
+            $('#mentionBgColor + input').val(bgColor);
+            updateStatus('Options saved.');
+        });
     }
 
     function resetOptions() {
+        chrome.storage.sync.clear(function () {
+            loadOptions();
+            updateStatus('Options reset.');
+        });
+    }
+
+    function updateStatus(message) {
+        var $status = $('#status');
+        $status.text(message);
+        setTimeout(function () {
+            $status.fadeOut('slow', function () {
+                $status.text('').show();
+            });
+        }, 2000);
     }
 });
